19c4fbbd5fcd5671f56fcc031c3058bfd87ed73e
Cabeda <jecabeda@gmail.com>
2021-05-02 16:17:13 +0100

    Fix for db uri in heroku

--- app/database.py
+++ app/database.py
@@ -3,9 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL")
+DB_URL = getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Fix needed as heroku returns DB starting with postgres://
+if DB_URL.startswith("postgres://"):
+    uri = DB_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(uri)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
